8090d2bc27a68cae9672881a202536b9f2c0fe37
theXappy <shaishapira@me.com>
2021-04-13 23:38:32 +0300

    Move PacketToSend to new file

--- ExtcapNet/PacketPublish/ExtendedPcapngPublisher.cs
+++ ExtcapNet/PacketPublish/ExtendedPcapngPublisher.cs
@@ -103,12 +103,4 @@ namespace ExtcapNet.PacketPublish
             GC.SuppressFinalize(this);
         }
     }
-
-    public struct PacketToSend
-    {
-        public byte[] Data { get; set; }
-        public LinkLayerType? LinkLayer { get; set; }
-        public TimeSpan? TimeFromCaptureStart { get; set; }
-        public string Comment { get; set; }
-    }
 }
---
+++ ExtcapNet/PacketPublish/PacketToSend.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtcapNet.PacketPublish
+{
+    public struct PacketToSend
+    {
+        public byte[] Data { get; set; }
+        public LinkLayerType? LinkLayer { get; set; }
+        public TimeSpan? TimeFromCaptureStart { get; set; }
+        public string Comment { get; set; }
+    }
+}
